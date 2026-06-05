a9c3b1bff52cff759214bfedfa092122550125c6
Rachelle Barraud <rbarraud@bne-sas.com>
2026-06-05 11:06:17 +0200

    Delete Projet 4 directory

--- Projet 4/match.py
+++
@@ -1,5 +0,0 @@
-class ChessMatch:
-    def __init__(self, white_player, black_player):
-        self.white_player = white_player
-        self.black_player = black_player
-        self.result = "en cours"
--- Projet 4/tournaments.py
+++
@@ -1,6 +0,0 @@
-class Tournament:
-    def __init__(self, location, starting_date, finish_date, number_of_rounds):
-        self.location = location
-        self.starting_date = starting_date
-        self.finish_date = finish_date
-        self.number_of_rounds = number_of_rounds
